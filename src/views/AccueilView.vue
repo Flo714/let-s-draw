06cafec0c9385448128c7e1d4b7b758152316521
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-17 21:12:16 +0200

    HTML - Page d'accueil sans les images

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -17,7 +17,68 @@
                     <p>S'inscrire</p>
                 </div>
             </nav>
-
         </div>
     </header>
+    <body>
+        <div>
+            <h1>Enfants & Artiste, présentez votre coup de pinceau, partagez votre talent sur Let’s Draw</h1>
+
+            <!-- Image début -->
+        </div>
+
+        <div>
+           <p>Envoyer mon dessin</p> 
+           <p>Faire un dessin</p> 
+        </div>
+        
+        <h2>Les Dernieres Tendances</h2>
+
+        <div>
+            <div>
+                <img src="" alt="">
+
+                <div>
+                    <p>Jean Vart</p>
+                    <p>Peintre</p>
+                </div>
+
+                <div>
+                    <p>7 Jours</p>
+                </div>
+                <div>
+                    <!-- Étoiles -->
+                </div>
+            </div>
+        </div>
+    </body>
+    <footer>
+        
+        <div>
+            <h3>Lien</h3>
+            <p>Faire un don</p>
+            <p>Tendances</p>
+            <p>Nous contacter</p>
+            <p>Glossaire</p>
+        </div>
+         <div>
+            <h3>Obtenir de l'aide</h3>
+            <p>Lets.Draw250@gmail.com</p>
+            <p>À propos</p>
+        </div>
+        <div>
+            <h3>Information</h3>
+            <p>Mentions Légales</p>
+        </div>
+        <div>
+            <h3>Suivez-nous</h3>
+            <!-- Logo Réseaux Sociaux -->
+        </div>
+        <div>
+            <!-- Image Paiement -->
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </footer>
 </template>

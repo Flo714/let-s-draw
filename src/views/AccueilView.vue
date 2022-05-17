fa064e2f024feeb4595cb97991bfc681003d89c6
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-17 20:46:54 +0200

    HTML - Header sans les logo

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>let's draw</h1>
+    <header>
+        <div>
+            <div>
+                <!-- Logo Let's Draw-->
+            </div>
+            <nav>
+                <p>Tendances</p>
+                <p>Glossaire</p>
+                <p>Nous Contacter</p>
+                <div>
+                    <!-- Logo don -->
+                    <p>Faire un logo</p>
+                </div>
+                <div>
+                    <!-- logo s'inscrire -->
+                    <p>S'inscrire</p>
+                </div>
+            </nav>
+
+        </div>
+    </header>
 </template>
